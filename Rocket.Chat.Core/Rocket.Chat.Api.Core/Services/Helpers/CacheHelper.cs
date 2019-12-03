762059fb660b61e9058f958d8fd9d94884470a17
Ignacio Burgos <ignacio.burgos-escribano@europol.europa.eu>
2019-12-03 17:38:30 +0100

    Delete CacheHelper

--- Rocket.Chat.Core/Rocket.Chat.Api.Core/Services/Helpers/CacheHelper.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Rocket.Chat.Api.Core.Services
-{
-    public class CacheHelper
-    {
-        public const string AUTHORIZED_KEY = "Authorized";
-        public const string USER_ID_KEY = "UserId";
-        public const string AUTH_TOKEN_KEY = "AuthToken";
-    }
-}
